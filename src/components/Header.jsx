81ed4f446c3b5fe17942ed9f3250225e07de8b08
navya9985 <navyagubala@gmail.com>
2025-12-04 20:29:14 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,54 +3,36 @@ import devlink from "../assets/devlink.png"
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false);
 
+const Header = () => {
   return (
     <header className="header">
 
       {/* LOGO + NAME */}
       <div className="left">
-        <img src={devlink} alt="logo" className="logo" />
+        <img src={devlink} className="logo" alt="logo" />
         <h2 className="name">DevLink</h2>
       </div>
 
-      {/* DESKTOP LINKS */}
+      {/* NAV LINKS */}
       <nav className="links">
         <a href="#home">Home</a>
-        <a href="#services">Services</a>
         <a href="#about">About</a>
+        <a href="#services">Services</a>
         <a href="#contact">Contact</a>
       </nav>
 
-      {/* DESKTOP LOGIN */}
+      {/* LOGIN BUTTON */}
       <div className="buttons">
         <Link to="/login">
           <button className="login">Login</button>
         </Link>
       </div>
 
-      {/* MOBILE HAMBURGER */}
-      <div className="hamburger" onClick={() => setOpenMenu(!openMenu)}>
-        ☰
-      </div>
-
-      {/* MOBILE MENU */}
-      {openMenu && (
-        <div className="mobile-menu">
-          <a href="#home" onClick={() => setOpenMenu(false)}>Home</a>
-          <a href="#services" onClick={() => setOpenMenu(false)}>Services</a>
-          <a href="#about" onClick={() => setOpenMenu(false)}>About</a>
-          <a href="#contact" onClick={() => setOpenMenu(false)}>Contact</a>
-
-          <Link to="/login">
-            <button className="login" onClick={() => setOpenMenu(false)}>Login</button>
-          </Link>
-        </div>
-      )}
-
     </header>
   );
 };
 
 export default Header;
+
+
